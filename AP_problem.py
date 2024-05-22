965621bff56dcb4e1a821ec4e46ce4dec8179fd0
Matthias-Rev <reveillard.matthias@outlook.fr>
2024-05-22 15:36:29 +0200

    Algo v2

--- AP_problem.py
+++ AP_problem.py
@@ -22,27 +22,34 @@ class AP():
         self.Mu = data['Mu']
     
     def AP_IPL(self):
-        # Unpack
+
         I = [i for i in range(len(self.R))]
         
         model = gp.Model('AP_IPL')
 
         # Variables
         y = {}
+        y_non_zero = {}
         
         for i in I:
-            y[i] = model.addVar(vtype=GRB.SEMIINT, name = "y[%s]" % i, lb=0)
+            y[i] = model.addVar(vtype=GRB.CONTINUOUS, name = "y[%s]" % i, lb=0)
 
-        # Second add the constraint of your model
-        model.addConstr(y[0]+gp.quicksum(y[i] for i in I) == 1) # Need to remove i=0
-        # model.addConstrs(y[i] <= y[0]*exp(Mu[i]) for i in I)
-        # model.addConstrs(y[i] <= z[i] for i in I)
+        for i in I:
+            y_non_zero[i] = model.addVar(vtype=GRB.BINARY)
+
+        Y_constrain = {}
+
+        for i in I:
+            Y_constrain[i] = model.addConstr(
+                y[i] <= y_non_zero[i]*y[0]*exp(self.Mu[i])
+            )
+
+        model.addConstr(y[0]+gp.quicksum(y[i] for i in I[1:]) == 1)
         
         # Then set the objective function
         model.setObjective(self.R[0]*y[0] + gp.quicksum(self.R[i]*y[i] for i in I[1:]), GRB.MAXIMIZE)
         # Update the model
         model.update()
-
 
         # Otpimize it
         model.optimize()
@@ -51,69 +58,17 @@ class AP():
 
         return model
 
-    def AP_LD(self):
-        # Unpack
-        self.R = data['R']
-        self.Mu = data['Mu']
-        I = [i for i in range(len(self.R))]
-        
-        model = gp.Model('AP_LD')
-
-        # First add the variables that you use
-        # y = model.addVars(len(R),vtype=GRB.CONTINUOUS, name = "y", lb=0)
-        # z = model.addVars(len(R),vtype=GRB.BINARY, name = "z")
-
-        # Variables
-        pi = {}
-        
-        pi[0] = 0
-        for i in I[1:]:
-            pi[i] = model.addVar(vtype=GRB.CONTINUOUS, name = "pi[%s]" % i, lb=0)
-
-        pi_zero = model.addVar(vtype=GRB.CONTINUOUS, name="pi_zero")
-
-        Pi_constrain = {}
-        Pi_zero_constrain = {}
-
-        for i in I:
-            Pi_constrain[i] = model.addConstr(
-                pi_zero - gp.quicksum(pi[i]*exp(self.Mu[i]) for i in I[1:]) >= self.R[0], name="Pi_zero_constrain[%i]" %i
-            )
-
-        for i in I:
-            Pi_zero_constrain[i] = model.addConstr(
-                pi_zero + pi[i] >= self.R[i]
-            )
-
-        model.setObjective(pi_zero, GRB.MINIMIZE)
-        # Update the model
-        model.update()
-
-
-        # Otpimize it
-        model.optimize()
-        print("Optimization on AP-LD is done. Objective function Value: %.2f " % model.ObjVal)
-
-        return model
-
     def AP_IP(self):
-        # Unpack
-        self.Mu = data['Mu']
+
         I = [i for i in range(len(self.R))]
         
         model = gp.Model('AP_IP')
-
-        # First add the variables that you use
-        # y = model.addVars(len(R),vtype=GRB.CONTINUOUS, name = "y", lb=0)
-        # z = model.addVars(len(R),vtype=GRB.BINARY, name = "z")
 
         # Variables
         x= {}
 
         # Introduce a new variable to allow division by non-constant
         z= {}
-        min_bound = 1
-        max_bound = 1+sum((exp(self.Mu[i]) for i in I[1:]))
         
         for i in I[1:]:
             x[i] = model.addVar(vtype=GRB.BINARY, name = "x[%s]" % i)
@@ -136,16 +91,10 @@ class AP():
         return model
 
     def AP_LD(self):
-        # Unpack
-        self.R = data['R']
-        self.Mu = data['Mu']
+
         I = [i for i in range(len(self.R))]
         
         model = gp.Model('AP_LD')
-
-        # First add the variables that you use
-        # y = model.addVars(len(R),vtype=GRB.CONTINUOUS, name = "y", lb=0)
-        # z = model.addVars(len(R),vtype=GRB.BINARY, name = "z")
 
         # Variables
         pi = {}
@@ -181,14 +130,9 @@ class AP():
         return model
 
     def AP_L(self):
-        # Unpack
         I = [i for i in range(len(self.R))]
         
         model = gp.Model('AP_L')
-
-        # First add the variables that you use
-        # y = model.addVars(len(R),vtype=GRB.CONTINUOUS, name = "y", lb=0)
-        # z = model.addVars(len(R),vtype=GRB.BINARY, name = "z")
 
         # Variables
         y = {}
@@ -203,10 +147,7 @@ class AP():
                 y[i] <= y[0]*exp(self.Mu[i])
             )
 
-        # Second add the constraint of your model
         model.addConstr(y[0]+gp.quicksum(y[i] for i in I[1:]) == 1) # Need to remove i=0
-        # model.addConstrs(y[i] <= y[0]*exp(Mu[i]) for i in I)
-        # model.addConstrs(y[i] <= z[i] for i in I)
         
         # Then set the objective function
         model.setObjective(self.R[0]*y[0] + gp.quicksum(self.R[i]*y[i] for i in I[1:]), GRB.MAXIMIZE)
@@ -221,23 +162,12 @@ class AP():
         return model
 
     def APC_MILP(self):
-        # Unpack
-        self.R = data['R']
-        self.Mu = data['Mu']
+
         I = [i for i in range(len(self.R))]
 
-        # Change here the probability --> Not a proba ! p is the number of objects. p \in {1, n/5, n/2, n}
         p = len(I)/5
         
         model = gp.Model('APC_MILP')
-
-        # First add the variables that you use
-        # y = model.addVars(len(R),vtype=GRB.CONTINUOUS, name = "y", lb=0)
-        # z = model.addVars(len(R),vtype=GRB.BINARY, name = "z")
-        
-        # That's the code from the lab
-        # however there are strange because you build your dictionnary with key equals to the value of R ?
-        # like y={"0.0":1, "0.0":2,...}
 
         # Variables
         y = {}
@@ -250,15 +180,8 @@ class AP():
             z[i] = model.addVar(vtype=GRB.BINARY, name = "z[%s]" % i)
 
         # 4 Constraints
-        Probability = {}
         Y_constrain = {}
         Z_constrain = {}
-        Unknow_constrain = {}
-
-        # for i in I:
-        #     Probability[i] = model.addConstr(
-        #         y[0] + gp.quicksum(y[i]) == 1
-        #     )
 
         for i in I:
             Y_constrain[i] = model.addConstr(
@@ -268,14 +191,9 @@ class AP():
         for i in I:
             Z_constrain[i] = model.addConstr(y[i] <= z[i])
 
-        # for i in I:
-        #     Unknow_constrain[i] = model.addConstr(gp.quicksum(z[i]) <= p)
-
-        # Second add the constraint of your model
-        model.addConstr(y[0]+gp.quicksum(y[i] for i in I[1:]) == 1) # Need to remove i=0
-        # model.addConstrs(y[i] <= y[0]*exp(Mu[i]) for i in I)
-        # model.addConstrs(y[i] <= z[i] for i in I)
-        model.addConstr(gp.quicksum(z[i] for i in I[1:]) <= p ) #need to remove i=0
+        # Constrain
+        model.addConstr(y[0]+gp.quicksum(y[i] for i in I[1:]) == 1)
+        model.addConstr(gp.quicksum(z[i] for i in I[1:]) <= p )
         
         # Then set the objective function
         model.setObjective(self.R[0]*y[0] + gp.quicksum(self.R[i]*y[i] for i in I[1:]), GRB.MAXIMIZE)
@@ -324,4 +242,6 @@ Problem_Ap = AP(data=data)
 # for v in model_AP_IP.getVars():
 #     print(f"{v.VarName} = {v.X}")
 model_AP_IPL= Problem_Ap.AP_IPL()
-print([j.X for j in model_AP_IPL.getVars()])
+names_to_retrieve = (f"y[{i}]" for i in range(len(data['R'])))
+y_value = [model_AP_IPL.getVarByName(name).X for name in names_to_retrieve]
+print(y_value)
